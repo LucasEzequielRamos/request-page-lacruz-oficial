9e7f9331c94104724486089d87d8ab410a5a6c0c
Lucas Becerra <lucaseramos13@gmail.com>
2024-06-04 20:24:08 -0300

    add telegram bot test

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,35 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
+export async function POST (req: NextRequest) {
+  try {
+    const msg = await req.json()
+    const { name, phone, content } = msg
+    console.log({ name, phone, content })
+
+    if (name === null || phone === null || content === null) {
+      return NextResponse.json({ message: 'empty name, phone or content' })
+    }
+
+    const queryParams = new URLSearchParams({
+      chat_id: '-1002149412259',
+      text: `<strong>Enviado por ${name}.\nSu número de contacto es ${phone}.\nPetición o agradecimiento: ${content}.\nEnviado \n</strong>`,
+      parse_mode: 'HTML'
+    }).toString()
+
+    const res = await fetch(`https://api.telegram.org/bot${process.env.TOKEN_BOT}/sendMessage?${queryParams}`)
+    const message = await res.json()
+    console.log(message)
+
+    return NextResponse.json(message)
+
+    // const message =
+    //     await sql`INSERT INTO messages (name, phone, content, timestamp) VALUES (${msg.name}, ${msg.phone}, ${msg.content}, CURRENT_TIMESTAMP - INTERVAL '3  hours') RETURNING *`
+
+    // console.log(message.rows)
+
+    // return NextResponse.json(message.rows)
+  } catch (error: any) {
+    console.log(error)
+    return NextResponse.json({ message: error.message })
+  }
+}
