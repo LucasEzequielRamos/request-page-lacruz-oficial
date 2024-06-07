eae6190ae34a950142a6e5a5bb460a923cb95a39
Lucas Becerra <lucaseramos13@gmail.com>
2024-06-06 21:00:56 -0300

    eliminate routes unusless

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import { useAppStatus } from './hooks/useStatusApp'
 // }
 const postEmail = async (formData: FormData) => {
   const contentForm = { name: formData.get('name'), phone: formData.get('phone'), content: formData.get('content') }
-  console.log(contentForm)
 
   const res = await fetch('/api/send-message', {
     method: 'POST',
@@ -62,7 +61,6 @@ export default function Home () {
       const formCurrent1 = formRef.current[0] as HTMLInputElement
       const formCurrent2 = formRef.current[1] as HTMLInputElement
       const formCurrent3 = formRef.current[2] as HTMLInputElement
-      console.log(formCurrent2.value)
       if (formCurrent1.value !== '' && formCurrent2.value !== '' && formCurrent3.value !== '') {
         setAppStatus(APP_STATUS.ALREADY_SEND)
       } else {
