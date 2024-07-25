f6f84217c6bb9f37a2a2f09379de6c9f7aa86843
Lucas Becerra <lucaseramos13@gmail.com>
2024-07-25 18:02:49 -0300

    add 1024p query, next image backgorund resize

--- src/app/components/main-section.tsx
+++ src/app/components/main-section.tsx
@@ -2,36 +2,35 @@ const MainSection = ({ setReason, REASONS }: { setReason: React.Dispatch<React.S
   return (
     <main className="flex flex-col  lg:mb-40 mb-20  ">
         <div className="flex justify-center py-10 items-center w-full">
-          <img className="lg:h-14 h-4 lg:mt-16" src="./arrow-down.webp" alt="Imagen de flecha para abajo" />
+          <img className="2xl:h-14 lg:h-8 h-4 lg:mt-16" src="./arrow-down.webp" alt="Imagen de flecha para abajo" />
           </div>
           <div className="flex flex-col lg:flex-row items-center lg:items-end ">
             <div className="flex flex-col text-center lg:w-1/2  ">
-            <h2 className=" lg:text-[5.5rem] text-[2.5rem] leading-[0.85] w-10/12 lg:mb-11 mb-5 m-auto mt-0">QUEREMOS <br/> ORAR CON VOS</h2>
-              <p className="lg:text-[1.7rem] text-[0.875rem] w-10/12 lg:w-7/12  m-auto mt-0 pb-12 lg:pb-24">Si nos ponemos de acuerdo en oración pidiéndole a
+            <h2 className=" 2xl:text-[5.5rem] lg:text-[3.5rem] text-[2.5rem] leading-[0.85] w-10/12 lg:mb-11 mb-5 m-auto mt-0">QUEREMOS <br/> ORAR CON VOS</h2>
+              <p className="2xl:text-[1.7rem] lg:text-[1.2rem] text-[0.875rem] w-10/12 lg:w-7/12  m-auto mt-0 pb-12 lg:pb-24">Si nos ponemos de acuerdo en oración pidiéndole a
               Dios lo que necesitamos, El lo hará. <br/> (Mateo 18:19)</p>
-              <button onClick={() => { setReason(REASONS.REQUEST) }} className="py-3   lg:py-7 px-2 bg-[#1C1D1E] w-4/5 lg:w-2/4  m-auto text-white rounded-[90px] lg:text-4xl text-2xl tracking-wide">PEDIDOS DE ORACIÓN</button>
-              <h3 className=" pt-2 max-sm:text-xs text-[#1C1D1E]/70">HACE CLICK PARA INGRESAR TU PEDIDO</h3>
+              <button onClick={() => { setReason(REASONS.REQUEST) }} className="py-3 2xl:py-7 px-2 bg-[#1C1D1E] w-4/5 lg:w-3/5 2xl:w-2/4 m-auto text-white rounded-[90px] 2xl:text-4xl text-2xl tracking-wide">PEDIDOS DE ORACIÓN</button>
+              <h3 className=" pt-2 max-2xl:text-xs text-[#1C1D1E]/70">HACE CLICK PARA INGRESAR TU PEDIDO</h3>
             </div>
-            <div className="lg:hidden flex flex-col items-center mt-20">
-          <h3 className="font-bold underline text-black text-[0.8rem] pb-2 w-full">¿En qué estado se encuentra tu pedido de oración?</h3>
-          <p className="pb-9 text-center text-sm">Seguimos orando juntos. <br /> Contanos en qué estado se encuentra tu pedido.</p>
-          <button onClick={() => { setReason(REASONS.UPDATE) }} className="py-3  bg-[#ef8100] px-5 m-auto text-white rounded-[90px] text-[1rem] tracking-wide ">ACTUALIZACIÓN DE PEDIDO</button>
-          <h3 className=" pt-2 text-[0.7rem] text-[#1C1D1E]/70 text-xs">HACE CLICK PARA INGRESAR TU ACTUALIZACIÓN</h3>
-
+            <div className="lg:hidden flex flex-col items-center text-center mt-20">
+              <h3 className="font-bold underline text-black max-md:text-[0.8rem] pb-2 w-full">¿En qué estado se encuentra tu pedido de oración?</h3>
+              <p className="pb-9 text-center max-md:text-sm">Seguimos orando juntos. <br /> Contanos en qué estado se encuentra tu pedido.</p>
+              <button onClick={() => { setReason(REASONS.UPDATE) }} className="py-3  bg-[#ef8100] px-5 m-auto text-white rounded-[90px] text-[1rem] tracking-wide ">ACTUALIZACIÓN DE PEDIDO</button>
+              <h3 className=" pt-2 text-[0.7rem] text-[#1C1D1E]/70 text-xs">HACE CLICK PARA INGRESAR TU ACTUALIZACIÓN</h3>
             </div>
-            <span className="lg:h-[520px] w-72 lg:w-0 lg:border-l-4 border-t-[1px] lg:border-t-0 mt-16  mb-9 lg:mb-0 lg:mt-28 border-black"></span>
+            <span className="2xl:h-[520px] lg:h-[370px] w-72 lg:w-0 lg:border-l-4 border-t-[1px] lg:border-t-0 mt-16  mb-9 lg:mb-0 lg:mt-28 border-black"></span>
             <div className="flex flex-col text-center lg:w-1/2">
-              <h2 className=" lg:text-[5.5rem] text-[2.5rem] leading-[0.85]  m-auto mt-0 lg:pb-24 pb-5">TESTIMONIOS</h2>
-              <p className="lg:text-[1.7rem] text-[0.875rem] lg:w-7/12 w-10/12 m-auto mt-0 pb-12">Dios es GRANDE, AMOROSO y FIEL. <br />Demos gracias por sus bondades en todo tiempo y recordemos lo que ya hizo en nuestras vidas. <br />(Salmos 100:4-5)</p>
-              <button onClick={() => { setReason(REASONS.GRATITUDE) }} className="py-3 lg:py-7 px-2  bg-[#1C1D1E] w-4/5 lg:w-2/4  m-auto text-white rounded-[90px] lg:text-4xl tracking-wide text-2xl">TESTIMONIOS</button>
-              <h3 className="max-sm:text-xs pt-2 text-[#1C1D1E]/70">HACE CLICK PARA INGRESAR TU AGRADECIMIENTO</h3>
+              <h2 className=" 2xl:text-[5.5rem] lg:text-[3.5rem] text-[2.5rem] leading-[0.85]  m-auto mt-0 2xl:pb-24 lg:pb-20 pb-5">TESTIMONIOS</h2>
+              <p className="2xl:text-[1.7rem] lg:text-[1.2rem] text-[0.875rem] 2xl:w-7/12 w-10/12 m-auto mt-0 pb-12 lg:min-h-[165px]">Dios es GRANDE, AMOROSO y FIEL. <br />Demos gracias por sus bondades en todo tiempo y recordemos lo que ya hizo en nuestras vidas. <br />(Salmos 100:4-5)</p>
+              <button onClick={() => { setReason(REASONS.GRATITUDE) }} className="py-3 2xl:py-7 px-2  bg-[#1C1D1E] w-4/5 lg:w-3/5 2xl:w-2/4 m-auto text-white rounded-[90px] 2xl:text-4xl tracking-wide text-2xl">TESTIMONIOS</button>
+              <h3 className="max-2xl:text-xs pt-2 text-[#1C1D1E]/70">HACE CLICK PARA INGRESAR TU AGRADECIMIENTO</h3>
             </div>
           </div>
           <div className="lg:flex hidden flex-col items-center mt-20">
-          <h3 className="font-bold underline text-black text-5xl pb-10">¿En qué estado se encuentra tu pedido de oración?</h3>
-          <p className="pb-10 text-center text-[2.5rem]">Seguimos orando juntos. <br /> Contanos en qué estado se encuentra tu pedido.</p>
-          <button onClick={() => { setReason(REASONS.UPDATE) }} className="py-6  bg-[#ef8100]  w-2/6  m-auto text-white rounded-[90px] text-[2.5rem] tracking-wide ">ACTUALIZACIÓN DE PEDIDO</button>
-          <h3 className=" pt-3 text-[#1C1D1E]/70">HACE CLICK PARA INGRESAR TU ACTUALIZACIÓN</h3>
+          <h3 className="font-bold underline text-black 2xl:text-5xl text-3xl pb-10">¿En qué estado se encuentra tu pedido de oración?</h3>
+          <p className="pb-10 text-center 2xl:text-[2.5rem] text-[1.5rem]">Seguimos orando juntos. <br /> Contanos en qué estado se encuentra tu pedido.</p>
+          <button onClick={() => { setReason(REASONS.UPDATE) }} className="2xl:py-6 py-3 bg-[#ef8100] w-2/6 m-auto text-white rounded-[90px] 2xl:text-[2.4rem] text-[1.4rem] tracking-wide ">ACTUALIZACIÓN DE PEDIDO</button>
+          <h3 className=" pt-2 max-2xl:text-xs text-[#1C1D1E]/70">HACE CLICK PARA INGRESAR TU ACTUALIZACIÓN</h3>
           </div>
       </main>
   )
--- src/app/components/message-form.tsx
+++ src/app/components/message-form.tsx
@@ -18,9 +18,9 @@ const MessageForm = ({ reason, setReason }: { reason: string, setReason: React.D
 
   return (
     <section className='flex bg-white/40 items-center justify-center fixed top-0 left-0 h-screen w-screen flex-col'>
-    <div className='flex  flex-col lg:min-h-[90%] lg:px-12 px-5 lg:w-7/12 w-10/12 bg-[#1C1D1E] rounded-[20px] lg:rounded-[90px] text-center text-white'>
+    <div className='flex  flex-col lg:min-h-[85%] lg:px-12 px-5 2xl:w-7/12 w-10/12 bg-[#1C1D1E] rounded-[20px] lg:rounded-[90px] text-center text-white'>
     <button onClick={() => { setReason('') }} className='lg:pt-10 pt-3 lg:pr-4 w-full flex justify-end '><img src="x.webp" className='max-sm:size-4' alt="x icon" /></button>
-    <h2 className='text-[#f5f5f5] font-extrabold lg:pb-12 lg:pt-7 pt-1 pb-5 w-full lg:text-7xl text-[1.25rem]'>{isReasonPedido === 1 ? 'QUEREMOS ORAR CON VOS' : isReasonPedido === 2 ? 'CELEBREMOS JUNTOS' : 'ACTUALIZACIÓN DE PEDIDO'}</h2>
+    <h2 className='text-[#f5f5f5] font-extrabold lg:pb-12 lg:pt-7 pt-1 pb-5 w-full 2xl:text-7xl lg:text-4xl text-[1.25rem]'>{isReasonPedido === 1 ? 'QUEREMOS ORAR CON VOS' : isReasonPedido === 2 ? 'CELEBREMOS JUNTOS' : 'ACTUALIZACIÓN DE PEDIDO'}</h2>
     <form ref={formRef} onSubmit={() => {
       setAppStatus(APP_STATUS.SENDING)
       reason === '' && setAppStatus(APP_STATUS.IDLE)
@@ -29,13 +29,13 @@ const MessageForm = ({ reason, setReason }: { reason: string, setReason: React.D
       formRef.current?.reset()
       setReason('')
     }}className='flex flex-col gap-5 w-full pb-5'>
-      <div className='flex lg:[&>input]:py-9 [&>input]:px-3 [&>input]:rounded-[50px] [&>input]:border-[#f5f5f5] [&>input]:border-solid [&>input]:border-4 [&>input]:bg-[#f5f5f5] [&>input]:text-black lg:gap-7 gap-1  '>
+      <div className='flex 2xl:[&>input]:py-9 lg:[&>input]:py-3 [&>input]:px-3 [&>input]:rounded-[50px] [&>input]:border-[#f5f5f5] [&>input]:border-solid [&>input]:border-4 [&>input]:bg-[#f5f5f5] [&>input]:text-black 2xl:gap-7 gap-1  '>
         <input aria-label='name-input' className='w-3/5 focus-visible:outline-none ' name='name' maxLength={20} placeholder='Nombre y apellido' type='text' onChange={() => { setAppStatus(APP_STATUS.TYPING) }} required/>
         <input aria-label='phone-input' className='w-2/5 focus-visible:outline-none ' name='phone' placeholder='Telefono' type='number' onChange={() => { setAppStatus(APP_STATUS.TYPING) }} required />
       </div>
-      <textarea aria-label='content-input' className='lg:h-[300px] h-44 lg:pt-14 px-3 pt-2 text-clip lg:rounded-[50px] rounded-[20px] text-black bg-[#f5f5f5] border-2 border-[#f5f5f5] border-solid focus-visible:outline-none resize-none' name='content' placeholder={`Escribe ${isReasonPedido === 1 ? `tu ${reason} de oracion` : isReasonPedido === 3 ? `la ${reason} de tu pedido` : `tu ${reason}`} aqui...`} onChange={() => { setAppStatus(APP_STATUS.TYPING) }} required/>
+      <textarea aria-label='content-input' className='2xl:h-[300px] lg:h-[200px] h-44 lg:pt-14 px-3 pt-2 text-clip lg:rounded-[50px] rounded-[20px] text-black bg-[#f5f5f5] border-2 border-[#f5f5f5] border-solid focus-visible:outline-none resize-none' name='content' placeholder={`Escribe ${isReasonPedido === 1 ? `tu ${reason} de oracion` : isReasonPedido === 3 ? `la ${reason} de tu pedido` : `tu ${reason}`} aqui...`} onChange={() => { setAppStatus(APP_STATUS.TYPING) }} required/>
       <div className='w-full flex justify-center'>
-      <button className={`text-[#f5f5f5] flex text-center justify-center w-full lg:text-4xl text-[1.25rem] lg:rounded-[50px] rounded-[30px] lg:py-5 py-2 bg-[#EF8100] lg:w-[30rem]  ${appStatus === APP_STATUS.IDLE || appStatus === APP_STATUS.SENDING ? 'pointer-events-none' : ''}`}
+      <button className={`text-[#f5f5f5] flex text-center justify-center w-full lg:text-4xl text-[1.25rem] lg:rounded-[50px] rounded-[30px] lg:py-5 py-2 bg-[#EF8100] lg:w-[30rem] hover:bg-[#EF8100]/90  ${appStatus === APP_STATUS.IDLE || appStatus === APP_STATUS.SENDING ? 'pointer-events-none bg-[#ccc]/70' : ''}`}
       disabled={isDisabled}>
         {appStatus === APP_STATUS.SENDING ? 'Enviando...' : `ENVIAR ${reason.toUpperCase()}`}
       </button>
